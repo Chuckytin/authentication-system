b537d0d8bf5b25b5a2ea471f79972f8dde3af10b
Chuckytin <angeltorcalcatalan@gmail.com>
2025-07-28 20:07:01 +0200

    v1.0.1 correción variable de entorno backend

--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -1,4 +1,4 @@
 export const AppConstants = {
-    BACKEND_URL: import.meta.env.BACKEND_URL || 'http://localhost:8080/api/v1'
+    BACKEND_URL: import.meta.env.URL_BACKEND || 'http://localhost:8080/api/v1'
 }
 //TODO: Añadir en el .env la URL del backend cuando esté desplegado
